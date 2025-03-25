dfd8c82e598eba4873f7ac12884604c2daf39507
Abhishek Rathore <abhishekrathore468@gmail.com>
2025-03-25 22:34:00 +0530

    firstCommit

--- server/index.js
+++ server/index.js
@@ -14,11 +14,15 @@ app.use(cors())
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
+app.get("/",(req,res)=>{
+    res.json({message:"start"})
+})
+
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 app.use("/api/auth",userRouter);
 app.use("/api/employee",employeeRouter);
 
 
-app.listen(port,()=>{
+app.listen(port,"0.0.0.0",()=>{
  console.log(`Server is running on https:localhost:${port}`)
 })
