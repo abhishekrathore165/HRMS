5a2d541909163335f98d1a60b9bf916b8a9212de
Abhishek Rathore <abhishekrathore468@gmail.com>
2025-03-25 22:20:54 +0530

    firstcommit

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,23 @@
 import pkg from "pg";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
-dotenv.config()
-const {Pool} = pkg ;
+dotenv.config();
+
+const { Pool } = pkg;
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false, 
+    ssl: {
+        rejectUnauthorized: false, // Required for some cloud providers
+    },
 });
-
 
 pool.connect((err, client, release) => {
     if (err) {
-        console.error(" Database connection failed:", err.message);
+        console.error("Database connection failed:", err.message);
     } else {
-        console.log(" Database connected successfully!");
-        release(); 
+        console.log("Database connected successfully!");
+        release();
     }
 });
 
