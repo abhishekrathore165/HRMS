168baf60bede67e875e4dc016aec012ab2ee61d0
Abhishek Rathore <abhishekrathore468@gmail.com>
2025-03-26 14:50:31 +0530

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,6 @@
+
+
+
 import pkg from "pg";
 import dotenv from "dotenv";
 
@@ -5,22 +8,20 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const isLocal = process.env.NODE_ENV === "development";
-
+console.log(process.env.DB_DATABASE_URL_LOCAL)
+ console.log(process.env.Data)
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: isLocal ? false : { rejectUnauthorized: false }, 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, 
+    },
 });
 
 pool.connect((err, client, release) => {
     if (err) {
         console.error("Database connection failed:", err.message);
     } else {
-        console.log(" Database connected successfully!");
+        console.log("Database connected successfully!");
         release();
     }
 });
